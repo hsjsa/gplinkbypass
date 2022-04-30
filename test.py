665d47366cc24c91e72229943bdf67ef88de114e
hsjsa <101786584+hsjsa@users.noreply.github.com>
2022-04-30 22:47:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ from telegram.ext import CommandHandler, run_async
 
 API_KEY = "5393668969:AAF4K7yPWA9_F3D0ibYgApSN0aFwdfj-8aw"
 
-@bot.messege_handler(commands=['nice'])
-def noice(messege):
-    bot.reply_to(messege,"congrats")
+@run_async
+def noice(update, context):
+    args = update.message.text.split(" ",maxsplit=1)
+    if len(args) > 1:
+        link = args[1]
+sendMessage(link, context.bot, update)
